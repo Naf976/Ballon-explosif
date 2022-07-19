018108f172fc6cb6929a0331d46ae959acdbbe46
Nafion Antoy <antoynafion@gmail.com>
2022-07-19 04:08:52 +0200

    Ajout de l'icone referesh, arreter de gonfler et changement de couleur du bouton quand le ballon à éclaté.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const ballon = document.querySelector("#ballon");
 const bouton = document.querySelector(".btn");
 let ballonWidth = 350;
+let refresh = document.querySelector("img");
 ballon.style.width = "350px";
 
 
-bouton.addEventListener("click", function gonfler() {
+if(ballonWidth < 500){ bouton.addEventListener("click", function gonfler() {
   ballonWidth += 20;
   ballon.style.width = ballonWidth + "px";
   ballon.style.transition = "0.5s";
@@ -13,9 +14,13 @@ bouton.addEventListener("click", function gonfler() {
     ballon.style.width = "0px";
     ballon.style.transition = "0s";
     let bravo = document.createElement("p");
-    bravo.innerHTML = "Bravo !!!!";
+    bravo.innerHTML = "BOOMM !!!";
     document.getElementById("conteneur").appendChild(bravo);
     bravo.classList.add("bravo");
     ballon.style.display = "none";
+    bouton.removeEventListener("click", gonfler);
+    bouton.style.backgroundColor = "#a0a0a0";
+    refresh.style.display = "block";
   }
-});
+})};
+console.log(bouton);

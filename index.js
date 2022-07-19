3d64581a6aa3baf2202c44eea243a60fd467704f
Nafion Antoy <antoynafion@gmail.com>
2022-07-19 15:05:02 +0200

    Création de la boucle lorsque l'utilisateur click sur le bouton refresh

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let refresh = document.querySelector("img");
 ballon.style.width = "350px";
 
 
-if(ballonWidth < 500){ bouton.addEventListener("click", function gonfler() {
+if(ballonWidth < 500){
+   bouton.addEventListener("click", function gonfler() {
   ballonWidth += 20;
   ballon.style.width = ballonWidth + "px";
   ballon.style.transition = "0.5s";
+  refresh.style.display = "none";
 
   if (ballonWidth > 500) {
     ballon.style.width = "0px";
@@ -20,7 +22,36 @@ if(ballonWidth < 500){ bouton.addEventListener("click", function gonfler() {
     ballon.style.display = "none";
     bouton.removeEventListener("click", gonfler);
     bouton.style.backgroundColor = "#a0a0a0";
-    refresh.style.display = "block";
+    refresh.style.display = "block"
   }
 })};
+let gonfler = function(){
+    
+}
+refresh.addEventListener("click", ()=>{
+  ballonWidth = 350;
+  ballon.style.width = ballonWidth + "px";
+  ballon.style.display = "block";
+document.querySelector(".bravo").remove();
+bouton.style.backgroundColor ="#7fffd4";
+bouton.addEventListener("click", function gonfler() {
+  ballonWidth += 20;
+  ballon.style.width = ballonWidth + "px";
+  ballon.style.transition = "0.5s";
+  refresh.style.display = "none";
+
+  if (ballonWidth > 500) {
+    ballon.style.width = "0px";
+    ballon.style.transition = "0s";
+    let bravo = document.createElement("p");
+    bravo.innerHTML = "BOOMM !!!";
+    document.getElementById("conteneur").appendChild(bravo);
+    bravo.classList.add("bravo");
+    ballon.style.display = "none";
+    bouton.removeEventListener("click", gonfler);
+    bouton.style.backgroundColor = "#a0a0a0";
+    refresh.style.display = "block"
+  }
+})
+})
 console.log(bouton);

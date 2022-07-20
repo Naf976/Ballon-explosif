bb179cbafc8cab32829ba9cab763af7e5901bc14
Nafion Antoy <antoynafion@gmail.com>
2022-07-20 04:05:31 +0200

    Ajout des fichiers audios (gonflement, éclatement), restylisation et changement du message des règle en fonction de l'état du ballon.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const ballon = document.querySelector("#ballon");
 const bouton = document.querySelector(".btn");
 let ballonWidth = 350;
-let refresh = document.querySelector("img");
+let refresh = document.querySelector(".refresh");
+let flash = document.querySelector(".flash");
 ballon.style.width = "350px";
+
 
 if (ballonWidth < 500) {
   gonfler()
@@ -14,35 +16,42 @@ function gonfler() {
     ballonWidth += 20;
     ballon.style.width = ballonWidth + "px";
     ballon.style.transition = "0.5s";
+    if (ballonWidth < 495) {
+      let gonflement = new Audio("son/gonflement.mp3");
+      gonflement.play();
+    }
+
 
 
     // Les actions quand le ballon éclate
     if (ballonWidth > 500) {
       ballon.style.width = "0px";
       ballon.style.transition = "0s";
-      let bravo = document.createElement("p");
-      bravo.innerHTML = "BOOMM !!!";
-      document.getElementById("conteneur").appendChild(bravo);
-      bravo.classList.add("bravo");
       ballon.style.display = "none";
       bouton.removeEventListener("click", gonflage);
       bouton.style.backgroundColor = "#a0a0a0";
       refresh.style.backgroundColor = "#fff";
+      let eclatement = new Audio("son/explosion.wav");
+      eclatement.play();
+      flash.style.display = "block";
+      setTimeout(()=>{flash.style.display = "none"}, 50);
+      document.querySelector(".regles").innerHTML = "Ah dommage il a éclaté !!";
 
       refresh.addEventListener("click", function initialiser() {
         ballonWidth = 350;
         ballon.style.width = ballonWidth + "px";
         ballon.style.display = "block";
-        document.querySelector(".bravo").remove();
         bouton.style.backgroundColor = "#7fffd4";
         refresh.removeEventListener("click", initialiser);
         refresh.style.backgroundColor = "#a0a0a0";
-      
+        flash.style.display = "none";
+        document.querySelector(".regles").innerHTML = "Appuis sur le bouton pour gonfler le ballon !";
+
         if (ballonWidth < 500) {
           gonfler();
         }
       });
-      
+
     }
   })
 };
